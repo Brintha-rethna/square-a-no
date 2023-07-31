2d998a1323cd8f250cbdb351b702e55e34433100
BRINTHA RETHNA M <119950256+Brintha-rethna@users.noreply.github.com>
2023-07-31 17:59:55 +0530

    Add files via upload

---
+++ square.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+function square()
+{
+var n=prompt ("Enter the limit 'n' to generate the table of numbers from 1 to n:","");
+var msg="";
+var res= "0";
+ 
+for(var x= 1; x<=n;x++)
+{
+res = x * x;
+msg = msg + " " + x + " * "+ x + " = " + res + "\n";
+}
+ 
+alert(msg);
+} 
+</script>
